0532160b316acd69c96f962636e60b6a1658eb83
chathuwick <35885787+chathuwick@users.noreply.github.com>
2018-07-15 13:47:51 +0300

    Dynamically pass the mongo endpoint
    Get the mongo endpoint from environmental variable

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
+var MONGO_URL =  process.env.MONGO_URL
 module.exports = {
     remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    localUrl: 'mongodb://'+MONGO_URL+'/meanstacktutorials'
 };
